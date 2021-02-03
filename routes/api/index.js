2cf802d9842ef849a8eb261f8080a19699204d99
oleg <vinodevmail@gmail.com>
2021-02-03 18:35:03 +0200

    basic routes added

---
+++ routes/api/index.js
@@ -0,0 +1,4 @@
+export { default as auth } from "./auth.js";
+export { default as posts } from "./posts.js";
+export { default as profile } from "./profile.js";
+export { default as users } from "./users.js";
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import connectDB from './db.js';
+import { auth, posts, profile, users } from './routes/api/index.js';
 
 const app = express();
 const port = process.env.port || 5000;
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+app.use('/api/auth', auth);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+app.use('/api/users', users);
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`)
