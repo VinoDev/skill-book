c25852349529b8193b934ae5e43882ebc771c1d9
oleg <vinodevmail@gmail.com>
2021-02-03 17:40:41 +0200

    added .env file and connected to mongodb

---
+++ db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        console.log('Mongodb connected...');
+    } catch(err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
+import express from 'express';
+import connectDB from './db.js';
 
 const app = express();
-
 const port = process.env.port || 5000;
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
