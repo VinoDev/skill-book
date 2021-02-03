4538b916a5056c338cac0d92dcd13ff877f075ab
oleg <vinodevmail@gmail.com>
2021-02-03 17:07:38 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.port || 5000;
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`)
+})
