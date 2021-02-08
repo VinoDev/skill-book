8d211c81d48c15b012629825f4201b9ce1765551
oleg <vinodevmail@gmail.com>
2021-02-08 15:03:08 +0200

    Add and remove experience from profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -105,4 +105,87 @@ router.get('/user/:user_id', async(req, res) => {
     }
 })
 
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+    '/experience', 
+    [
+        auth,
+        [
+            check('title', 'Title is required').notEmpty(),
+            check('company', 'Company is required').notEmpty(),
+            check('from', 'From date is required').notEmpty()
+        ]
+    ], 
+    async(req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newExp = {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        }
+
+        try {
+            const profile = await Profile.findOne({
+                user: req.user.id
+            })
+            if(!profile){
+                return res.status(400).json({ msg: 'Profile not found' });
+            }
+            profile.experience.unshift(newExp);
+            await profile.save()
+
+            res.json(profile);
+        } catch(error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+})
+
+// @route   PUT api/profile/experience/:exp_id
+// @desc    Remove expirience from profile
+// @access  Private
+router.put('/experience/:exp_id', auth, async(req, res) => {
+        try {
+            const profile = await Profile.findOne({
+                user: req.user.id
+            })
+            if(!profile){
+                return res.status(400).json({ msg: 'Profile not found' });
+            }
+            if(profile.experience.length === 0){
+                return res.status(400).json({ msg: 'This profile has no experience.' });
+            }
+
+            const removeIndex = profile.experience
+                .map(exp => exp.id)
+                .indexOf(req.params.exp_id);
+            
+            profile.experience.splice(removeIndex, 1);
+
+            await profile.save()
+            res.json(profile);
+        } catch(error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+})
 export default router;
