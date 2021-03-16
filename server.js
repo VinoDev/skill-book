586ed6d5ec446fb9dd4bd3b1085dcc2cafa4e880
oleg <vinodevmail@gmail.com>
2021-03-16 18:43:06 +0200

    port typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import { auth, post, profile, user } from './routes/api/index.js';
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 connectDB();
 
